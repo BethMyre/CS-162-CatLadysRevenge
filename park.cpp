a9867d1cf1dacaba46a0d88ccb4a03b387981fe9
BethMyre <myreel@oregonstate.edu>
2019-03-16 16:17:39 -0400

    Park - Reworked if/else order.

--- park.cpp
+++ park.cpp
@@ -27,31 +27,31 @@ int Park::action() {
     std::cout << std::endl;
     while (selection != 1) {
         std::cout << "What would you like to do?" << std::endl;
-        if (hereBefore == 0 || (hereBefore == 1 && catHere == 0)) {
-            std::cout << "1. Leave the park." << std::endl;
-            selection = intValidate(1, 1);
-        }
-        else if (hereBefore == 1 && catHere == 1) {
+        if (hereBefore == 1 && catHere == 1) {
             std::cout << "1. Leave the park." << std::endl;
             std::cout << "2. Try to get Snowball out of the tree." << std::endl;
             selection = intValidate(1, 2);
-        }
-        if (selection == 2) {
-            std::cout << "You approach the tree.  Snowball climbs even higher, clearly terrified." << std::endl;
-            std::cout << "\"Here kitty kitty!\" you call." << std::endl;
-            if (theBag->getTuna() == 0) {
-                std::cout << "She doesn't move.  "
-                    << "If only you had some way to tempt her to come down." << std::endl << std::endl;
+            if (selection == 2) {
+                std::cout << "You approach the tree.  Snowball climbs even higher, clearly terrified." << std::endl;
+                std::cout << "\"Here kitty kitty!\" you call." << std::endl;
+                if (theBag->getTuna() == 0) {
+                    std::cout << "She doesn't move.  "
+                              << "If only you had some way to tempt her to come down." << std::endl << std::endl;
+                }
+                else {
+                    std::cout << "You can tell Snowball needs extra encouragement, so you take the can " << std::endl;
+                    std::cout << "of tuna out of your purse and open it.  Snowball slowly climbs down the tree." << std::endl;
+                    std::cout << "You let her eat it, then scoop her up and put her into your purse." << std::endl << std::endl;
+                    theBag->setTuna(0);
+                    theBag->setSnowball(1);
+                    catHere = 0;
+                }
             }
-            else {
-                std::cout << "You can tell Snowball needs extra encouragement, so you take the can " << std::endl;
-                std::cout << "of tuna out of your purse and open it.  Snowball slowly climbs down the tree." << std::endl;
-                std::cout << "You let her eat it, then scoop her up and put her into your purse." << std::endl << std::endl;
-                theBag->setTuna(0);
-                theBag->setSnowball(1);
-            }
         }
-
+        else {
+            std::cout << "1. Leave the park." << std::endl;
+            selection = intValidate(1, 1);
+        }
     }
     std::cout << "Where would you like to go?" << std::endl;
     if (hereBefore == 0) {
@@ -60,10 +60,10 @@ int Park::action() {
         hereBefore = 1;
         return 2;
     }
-    else{
+    else {
         std::cout << "1. To the street." << std::endl;
         std::cout << "2. Home." << std::endl;
-        std::cout << "3. To the casino." << std::endl;
+        std::cout << "3. To the restaurant." << std::endl;
         int choice = intValidate(1, 3);
         return choice;
     }
